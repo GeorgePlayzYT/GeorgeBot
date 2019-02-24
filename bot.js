a014929b2558a7961225a1434fd944d53e808306
GeorgePlayzYT <47942664+GeorgePlayzYT@users.noreply.github.com>
2019-02-24 19:48:53 +0700

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,6 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+bot.login('NTQ4NDYyMjc2NTcxODI0MTI4.D1Puzg.JJIyzIQPUb5URnijHFq4NRIk81U')
+bot.on("ready",function(){
+    console.log("Ready");
+    client.login(process.evn.
